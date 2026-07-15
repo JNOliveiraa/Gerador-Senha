3595b9e1d2aef57473144ca3a5bf144e3ad09a92
Jean Oliveira <jeanoliveira2709@gmail.com>
2026-07-14 23:11:25 -0300

    adicionando novas opções.

--- gerador_senhas.py
+++ gerador_senhas.py
@@ -3,10 +3,20 @@ import string
 import streamlit as st
 
 
-def gerar_senha(comprimento, usar_especiais): 
-   caracteres = string.ascii_letters + string.digits
-   if usar_especiais: 
-      caracteres += string.punctuation 
+def gerar_senha(comprimento, usar_especiais, usar_numeros, apenas_numeros): 
+   # se o usuário escolheu "Apenas numeros", ignoramos letras e simbolos
+   if apenas_numeros:
+      caracteres = string.digits
+   else:
+      # começa com letras maiusculas e minusculas 
+      caracteres = string.ascii_letters
+   
+      if usar_numeros: 
+          caracteres += string.digits
+
+      if usar_especiais: 
+         caracteres += string.punctuation 
+
    return "".join(random.choice(caracteres) for _ in range (comprimento))
 
 
@@ -21,19 +31,27 @@ tamanho = st.slider (
    "Escolha o tamanho da senha: ", min_value=4, max_value=100, value=12
 )
 
-# caixa de seleção para símbolos
-especiais = st.checkbox (
-   "Incluir símbolos especiais?", help="Ex: !, @, #"
+col1, col2, col3 = st.columns(3)
+
+with col1: 
+   numeros = st.checkbox("Incluir Numeros", value= True)
+
+with col2:
+   especiais = st.checkbox (
+   "Incluir símbolos especiais?", help="Ex: !, @, #", value= True
 )
+
+with col3: 
+   apenas_numeros = st.checkbox("Apenas Números", value= False)
 
 st.divider()
 
 # Botão para gerar
 if st.button("Gerar Nova Senha", type = "primary"):
-   nova_senha = gerar_senha(tamanho, especiais)
+   nova_senha = gerar_senha(tamanho, especiais, numeros, apenas_numeros)
 
    st.success("Sua senha foi gerada com sucesso!")
-   st.code(nova_senha, language="txt") # caixa de texto pronta para copiar
+   st.code(nova_senha, language="text") # caixa de texto pronta para copiar
 
    # Salva no histórico Local automaticamente
    with open("senhas_web.txt", "a", encoding= "utf-8") as arquivo:
